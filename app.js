c01ecd698e4ee7602b67024054937aaed64b03de
Yamanadm <adsamu44@gmail.com>
2022-09-19 11:29:18 +0300

    Finished

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const input = document.querySelector("#number");
 const submit = document.querySelector("#submit");
 const restart = document.querySelector("#restart");
 const resP = document.querySelector("#res");
-let rightNumber = document.querySelector("#rightNumber");
+let secret = document.querySelector("#secret");
+
 let knewRes = document.querySelector("#knewRes");
 console.log(rightNumber);
 
 let right = 10;
 
-let secretNumber = Math.floor(Math.random() * 101) + 1;
+let secretNumber = Math.floor(Math.random() * 100) + 1;
 console.log(secretNumber);
 
 submit.addEventListener("click", (e) => {
@@ -17,44 +18,52 @@ submit.addEventListener("click", (e) => {
     alert("You should enter a number");
   } else {
     if (input.value == secretNumber) {
-      resP.innerHTML = `Awesome, You <span>knew</span>`;
-      knewRes.innerHTML = `You knew ${right}.time`;
+      right -= 1;
+      resP.innerHTML = `Awesome, You <span>knew <i class="fa-regular fa-face-smile icon"></i></span>`;
+      knewRes.innerHTML = `Score : ${right} point`;
+      secret.innerHTML = secretNumber;
       submit.disabled = true;
       input.value = "";
     } else if (input.value > secretNumber) {
-      resP.innerHTML = `You should <span>decrease <i class="fa-solid fa-arrow-down icon"></i></span>`;
       right -= 1;
-      rightNumber.innerHTML = right;
+      resP.innerHTML = `You should <span>decrease <i class="fa-solid fa-arrow-down icon"></i></span>`;
+      knewRes.innerHTML = `Score : ${right} point`;
       input.value = "";
       input.focus();
     } else if (input.value < secretNumber) {
-      resP.innerHTML = `You should <span>increase <i class="fa-solid fa-arrow-up icon"></i></span>`;
       right -= 1;
-      rightNumber.innerHTML = right;
+      resP.innerHTML = `You should <span>increase <i class="fa-solid fa-arrow-up icon"></i></span>`;
+      knewRes.innerHTML = `Score : ${right} point`;
+
+      //   rightNumber.innerHTML = right;
       input.value = "";
       input.focus();
     }
   }
   if (right === 0) {
-    resP.innerHTML = `Unfortunately, You <span>didn't knew</span>`;
+    resP.innerHTML = `Unfortunately, You <span>didn't knew <i class="fa-regular fa-face-frown icon"></i></span>`;
     knewRes.innerHTML = `It's not your lucky day.`;
+    secret.innerHTML = secretNumber;
     submit.disabled = true;
-    restart.disabled = true;
+    restart.disabled = false;
+    input.classList.remove("submit:hover");
   }
-});
 
-restart.addEventListener("click", (e) => {
-  e.preventDefault();
-  input.value = "";
-  input.focus();
-  right = 10;
-  secretNumber = Math.floor(Math.random() * 101) + 1;
-  console.log(secretNumber);
-  submit.disabled = false;
-  restart.disabled = false;
-  resP.innerHTML = "";
-  rightNumber.innerHTML = right;
-  knewRes = "You have <span>10</span> rights to find the number";
+  restart.addEventListener("click", (e) => {
+    e.preventDefault();
+    input.value = "";
+    input.focus();
+    right = 10;
+    knewRes.innerHTML = right;
+    secret.innerHTML = `<span>?</span>`;
+    secretNumber = Math.floor(Math.random() * 100) + 1;
+    console.log(secretNumber);
+    submit.disabled = false;
+    restart.disabled = false;
+    resP.innerHTML = "";
+
+    knewRes.innerHTML = `You have <span id="rightNumber">${right}</span> rights to find the number`;
+  });
 });
 
 window.addEventListener("load", () => {
